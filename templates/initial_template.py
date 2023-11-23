9e2a9a20375bd52a496168a78e74f6823c19df14
redohawk <131167893+redohawk@users.noreply.github.com>
2023-11-22 23:27:23 -0500

    Update initial_template.py - se corrige script y se mejoraidentación

--- templates/initial_template.py
+++ templates/initial_template.py
@@ -6,8 +6,8 @@ from PIL import Image, ImageTk
 from utils.template_handler import templ_handler
 
 def initial_templ(dinamic_frame):
-    #   Primera Ventana | Inicio
-     
+    # Primera Ventana | Inicio
+     font = "Helvetica 11"
      intro_txt = textwrap.dedent( """
                     Nuestro restaurante es un lugar donde ofrecemos
                     una variedad de platos deliciosos y recursos
@@ -15,41 +15,25 @@ def initial_templ(dinamic_frame):
                     necesidades culinarias y hacerte disfrutar de 
                     una experiencia gastronómica excepcional.
                     """)
-     lbl_intro_txt = Label(dinamic_frame, text = intro_txt,\
-          font = "Helvetica 11", anchor="center", justify="center")
-     
-     bttn_signin = Button(dinamic_frame, 
-                         text = "Registrarse",
-                         font = "Helvetica 11", 
-                         bg = "gray", 
-                         fg = "white", 
-                         command = lambda frame=dinamic_frame: templ_handler('signin', frame))
-     
+     lbl_intro_txt = Label(dinamic_frame, 
+                           text = intro_txt,
+                           font = font, 
+                           anchor="center", 
+                           justify="center")
+     bttn_signin = Button(dinamic_frame,
+                          text = "Registrarse",
+                          font = font, 
+                          bg = "gray", 
+                          fg = "white", 
+                          command = lambda frame=dinamic_frame: templ_handler('signin', frame))
      bttn_login = Button(dinamic_frame, 
                          text = "Iniciar Sesion",
-                         font = "Helvetica 11", 
+                         font = font, 
                          bg = "gray", 
                          fg = "white", 
-                         command = lambda frame=dinamic_frame: templ_handler(frame, 'login'))
-
+                         command = lambda frame=dinamic_frame: templ_handler('login', frame))
      lbl_intro_txt.grid(column=0, row=0, columnspan=2)
-     bttn_signin.grid(column=0, row=0, sticky="nsew", padx=10, pady=10)
+     bttn_signin.grid(column=0, row=1, sticky="nsew", padx=10, pady=10)
      bttn_login.grid(column=1, row=1, sticky="nsew", padx=10, pady=10)
-
-
-
-# from tkinter import *
-# from tkinter import ttk
-# import handler
-
-# def templ1(frame):
-
-#      ttk.Label(frame, text="plantilla 1").grid(column=0, row=0, columnspan=2)
-
-#      ttk.Button(frame, text="t2", command=lambda frame=frame: 
-#                              handler.handy('t2', frame)).grid(column=0, row=1)
-#      ttk.Button(frame, text="t3", command=lambda frame=frame: 
-#                              handler.handy('t3', frame)).grid(column=1, row=1)
-     
 
      
