1c49d1a93bb8f74f09fa331bfbd8a80733d52a34
redohawk <131167893+redohawk@users.noreply.github.com>
2023-11-23 00:21:57 -0500

    Update login_template.py - se corrige identacion y de dinamic_frame a dynamic_frame

--- templates/login_template.py
+++ templates/login_template.py
@@ -4,34 +4,34 @@ from PIL import Image, ImageTk
 
 from utils.template_handler import templ_handler
 
-def login_template(dinamic_frame):
+def login_template(dynamic_frame):
      #   Tercera  Ventana | Inicio Sesion
      font = "Helvetica 11"
      title_font = "Helvetica 14"
-     lbl_title = Label(dinamic_frame, 
+     lbl_title = Label(dynamic_frame, 
                        text="Inicio Sesión",
                        font=title_font)
-     lbl_subtitle_email = Label(dinamic_frame, 
+     lbl_subtitle_email = Label(dynamic_frame, 
                                 text="Email",
                                 font=font)
-     lbl_subtitle_pass = Label(dinamic_frame,
+     lbl_subtitle_pass = Label(dynamic_frame,
                                 text="Contraseña",
                                 font=font)
 
      user_email = StringVar()
      user_pass = StringVar()
      
-     entry_email = Entry(dinamic_frame, 
-                    textvariable=user_email)
-     entry_passw = Entry(dinamic_frame, 
-                    textvariable=user_pass, 
-                    show="*")
-     button_login = Button(dinamic_frame, 
+     entry_email = Entry(dynamic_frame, 
+                         textvariable=user_email)
+     entry_passw = Entry(dynamic_frame, 
+                         textvariable=user_pass, 
+                         show="*")
+     button_login = Button(dynamic_frame, 
                            text="Iniciar Sesión",
                            font=font, 
                            bg="gray", 
                            fg="white",
-                           command=lambda frame=dinamic_frame : templ_handler('login', frame)) #pendiente funcion que busque en la base de datos y de acceso
+                           command=lambda frame=dynamic_frame : templ_handler('login', frame)) #pendiente funcion que busque en la base de datos y de acceso
      
 
      lbl_title.grid(column=0, row=0, pady=10)
