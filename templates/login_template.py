7e4bf3544a9df703e5a870e9b0c0fdd66f044c00
redohawk <131167893+redohawk@users.noreply.github.com>
2023-11-23 00:04:06 -0500

    Update login_template.py - se reformula la funcionalidad del script

--- templates/login_template.py
+++ templates/login_template.py
@@ -1,48 +1,44 @@
 from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
-from utils.clean_frame import clean
+
 from utils.template_handler import templ_handler
 
-#   Ventana de Inicio Sesion                    3 Pag
-def login_template(root):
-     #   Tercera  Ventana | Inicio Sesio
-     clean(root)
-
-     login_templ_content = ttk.Frame(root)
-     login_templ_content.grid_rowconfigure(0, weight=1)
-     login_templ_content.grid_columnconfigure(0, weight=1)
-     #login_templ_content.grid_columnconfigure(1, weight=1)
-
-     image = Image.open("../multimedia/Logo.png")
-     image = image.resize((50, 50))
-     img = ImageTk.PhotoImage(image)
-     lbl_img = Label(login_templ_content, image = img)
-     lbl_ini = Label(login_templ_content, text = "Mi Restaurante",
-                    font = "Helvetica 14", anchor="center", justify="center")
-     lbl1 = Label(login_templ_content, text = "Inicio Sesión", 
-                    font = "Helvetica 11")
-     lbl2 = Label(login_templ_content, text = "Email")
-     lbl3 = Label(login_templ_content, text = "Contraseña")
+def login_template(dinamic_frame):
+     #   Tercera  Ventana | Inicio Sesion
+     font = "Helvetica 11"
+     title_font = "Helvetica 14"
+     lbl_title = Label(dinamic_frame, 
+                       text = "Inicio Sesión",
+                       font = title_font)
+     lbl_subtitle_email = Label(dinamic_frame, 
+                                text = "Email",
+                                font = font)
+     lbl_subtitle_pass = Label(dinamic_frame,
+                                text = "Contraseña",
+                                font = font)
 
      user_email = StringVar()
      user_pass = StringVar()
-     nemail = Entry(login_templ_content, textvariable=user_email)
-     npassw = Entry(login_templ_content, textvariable=user_pass, show="*")
-     button_loged = Button(login_templ_content, text = "Iniciar Sesión",
-                         font = "Helvetica 10", bg = "gray", fg = "white",
-                         command = lambda:templ_handler(root, 'initial'))#, command = lambda: initial_templ(root))
      
-     login_templ_content.grid(column=0, row=0, padx=10, pady=10,
-                                        sticky="nsew")
-     lbl_img.grid(column=0, row=0, pady=10)
-     lbl_ini.grid(column=0, row=1)
-     lbl1.grid(column=0, row=2, pady=10)
-     lbl2.grid(column=0, row=3)
-     nemail.grid(column=0, row=4)
-     lbl3.grid(column=0, row=5)
-     npassw.grid(column=0, row=6)
-     button_loged.grid(column=0, row=7, pady=10)
+     entry_email = Entry(dinamic_frame, 
+                    textvariable=user_email)
+     entry_passw = Entry(dinamic_frame, 
+                    textvariable=user_pass, 
+                    show="*")
+     button_login = Button(dinamic_frame, 
+                           text = "Iniciar Sesión",
+                           font = font, 
+                           bg = "gray", 
+                           fg = "white",
+                           command = lambda frame=dinamic_frame: templ_handler('login', frame)) #pendiente funcion que busque en la base de datos y de acceso
+     
+
+     lbl_title.grid(column=0, row=0, pady=10)
+     lbl_subtitle_email.grid(column=0, row=1)
+     entry_email.grid(column=0, row=2)
+     lbl_subtitle_pass.grid(column=0, row=3)
+     entry_passw.grid(column=0, row=4)
+     button_login.grid(column=0, row=5, pady=10)
 
 
-     #root.mainloop()
