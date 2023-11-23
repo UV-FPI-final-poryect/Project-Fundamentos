cf5e337e33c795c5efc5342bd9a59fe79ffcec7f
redohawk <131167893+redohawk@users.noreply.github.com>
2023-11-23 00:20:56 -0500

    Update initial_template.py - se corrige dinamic_frame a dynamic_frame

--- templates/initial_template.py
+++ templates/initial_template.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageTk
 
 from utils.template_handler import templ_handler
 
-def initial_templ(dinamic_frame):
+def initial_templ(dynamic_frame):
     # Primera Ventana | Inicio
      font = "Helvetica 11"
      intro_txt = textwrap.dedent( """
@@ -15,23 +15,23 @@ def initial_templ(dinamic_frame):
                     necesidades culinarias y hacerte disfrutar de 
                     una experiencia gastronómica excepcional.
                     """)
-     lbl_intro_txt = Label(dinamic_frame, 
+     lbl_intro_txt = Label(dynamic_frame, 
                            text = intro_txt,
                            font = font, 
                            anchor="center", 
                            justify="center")
-     bttn_signin = Button(dinamic_frame,
+     bttn_signin = Button(dynamic_frame,
                           text = "Registrarse",
                           font = font, 
                           bg = "gray", 
                           fg = "white", 
-                          command = lambda frame=dinamic_frame: templ_handler('signin', frame))
-     bttn_login = Button(dinamic_frame, 
+                          command = lambda frame=dynamic_frame: templ_handler('signin', frame))
+     bttn_login = Button(dynamic_frame, 
                          text = "Iniciar Sesion",
                          font = font, 
                          bg = "gray", 
                          fg = "white", 
-                         command = lambda frame=dinamic_frame: templ_handler('login', frame))
+                         command = lambda frame=dynamic_frame: templ_handler('login', frame))
      lbl_intro_txt.grid(column=0, row=0, columnspan=2)
      bttn_signin.grid(column=0, row=1, sticky="nsew", padx=10, pady=10)
      bttn_login.grid(column=1, row=1, sticky="nsew", padx=10, pady=10)
