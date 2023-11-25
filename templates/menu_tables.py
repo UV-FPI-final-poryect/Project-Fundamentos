45668a6c01a38f07ce33dd783f8a3d7ee0c60a07
SantiagoLopezUV <santiago.lopez.ramirez@correounivalle.edu.co>
2023-11-25 16:05:16 -0500

    GUI Update

--- templates/menu_tables.py
+++ templates/menu_tables.py
@@ -7,9 +7,11 @@ import utils.template_handler
 def menu_tables_template(dynamic_frame):
         font = "Helvetica 11"
         title_font = "Helvetica 14"
+        background = 'gray75'
         lbl_title = Label(dynamic_frame,
                         text="Gestion de Mesas",
-                        font=title_font)
+                        font=title_font,
+                        background=background)
         button_add_table = Button(dynamic_frame, text="Add Tables",\
                 font=font, bg="gray", fg="white", command = lambda \
                         frame=dynamic_frame: utils.template_handler.\
@@ -22,7 +24,13 @@ def menu_tables_template(dynamic_frame):
                 font=font, bg="gray", fg="white", command = lambda \
                         frame=dynamic_frame: utils.template_handler.\
                                 templ_handler('upd_table', frame))
+        button_return = Button(dynamic_frame, text = "BACK", font=font,
+                        bg="gray", fg="white",command = lambda \
+                        frame=dynamic_frame: utils.template_handler.\
+                                templ_handler('main_menu', frame))
+
         lbl_title.grid(column=0, row=0, pady=10)
         button_add_table.grid(column=0, row=1, pady=10)
         button_del_table.grid(column=0, row=2, pady=10)
         button_upd_table.grid(column=0, row=3, pady=10)
+        button_return.grid(column=0, row=4, pady=10)
