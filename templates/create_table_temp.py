2342a3bf3f8b7ed6e194ae97abb41bfc4fe2e882
SantiagoLopezUV <santiago.lopez.ramirez@correounivalle.edu.co>
2023-11-28 11:14:01 -0500

    Create Function Tables

--- templates/create_table_temp.py
+++ templates/create_table_temp.py
@@ -5,8 +5,8 @@ import utils.template_handler
 
 
 def create_table(dynamic_frame):
-
-    lbl_title = ttk.Label(dynamic_frame, text="Agregar Mesa")
+    lbl_title = ttk.Label(dynamic_frame, text="Agregar Mesa",
+                        font=("default",12, "bold"))
     lbl_date = ttk.Label(dynamic_frame, text="Date")
     date = StringVar()
     entry_date = ttk.Entry(dynamic_frame, textvariable=date)
@@ -16,15 +16,19 @@ def create_table(dynamic_frame):
     lbl_people = ttk.Label(dynamic_frame, text="Numbers of People")
     people = StringVar()
     entry_people = ttk.Entry(dynamic_frame, textvariable=people)
-    button_add = ttk.Button(dynamic_frame, text = "Add",
+    button_add = ttk.Button(dynamic_frame, text = "Add", style="Accent.TButton",
+                            command=lambda frame=dynamic_frame :\
+                                utils.template_handler.templ_handler('menu_tables', frame))
+    button_back = ttk.Button(dynamic_frame, text = "Back",
                         command=lambda frame=dynamic_frame :\
-            utils.template_handler.templ_handler('menu_tables', frame))
+                                utils.template_handler.templ_handler('menu_tables', frame))
 
-    lbl_title.grid(column=1, row=0, padx=20, pady=10)
-    lbl_date.grid(column=0, row=1, padx=10, pady=0)
-    entry_date.grid(column=0, row=2, padx=10, pady=10)
-    lbl_hour.grid(column=2, row=1, padx=10, pady=0)
-    entry_hour.grid(column=2, row=2, padx=10, pady=0)
-    lbl_people.grid(column=0, row=3, padx=10, pady=10)
-    entry_people.grid(column=0, row=4, padx=10, pady=0)
-    button_add.grid(column=1, row=5, pady=10)
+    lbl_title.grid(column=0, row=0, columnspan=2, padx=40, pady=10)
+    lbl_date.grid(column=0, row=1, sticky="w", padx=(20,0), pady=2)
+    entry_date.grid(column=0, row=2, padx=(20,10), pady=2)
+    lbl_hour.grid(column=1, row=1, sticky="w", padx=(10,0), pady=2)
+    entry_hour.grid(column=1, row=2, padx=(10,20), pady=2)
+    lbl_people.grid(column=0, row=3, sticky="w", padx=(20,0), pady=2)
+    entry_people.grid(column=0, row=4, padx=(20,10), pady=2)
+    button_add.grid(column=1, row=5, sticky="w", padx=(0,10), pady=15)
+    button_back.grid(column=0, row=5, sticky="e", padx=(0,10), pady=15)
--- templates/update_table_temp.py
+++ templates/update_table_temp.py
@@ -1,21 +1,22 @@
 from tkinter import *
-import tkinter as tk
+from tkinter import ttk
+
 from PIL import Image, ImageTk
 import utils.template_handler
 
 
 def update_table(dynamic_frame):
-    font = "Helvetica 11"
-    title_font = "Helvetica 14"
-    background = 'gray75'
-    lbl_title = tk.Label(dynamic_frame, text="Mesas",\
-        font=title_font, background=background)
-    button_del = Button(dynamic_frame, text = "Eliminar", font = font,\
-        bg = "gray", fg = "white", command=lambda frame=dynamic_frame :\
-            utils.template_handler.templ_handler('menu_tables', frame))
-    button_upd = Button(dynamic_frame, text = "Actualizar", font = font,\
-        bg = "gray", fg = "white", command=lambda frame=dynamic_frame :\
-            utils.template_handler.templ_handler('menu_tables', frame))
+    lbl_title = ttk.Label(dynamic_frame, text="Mesas",
+                        font=("default", 12, "bold"))
+
+    button_back = ttk.Button(dynamic_frame, text = "Back",
+                        command=lambda frame=dynamic_frame :\
+                                utils.template_handler.templ_handler('menu_tables', frame))
+    button_upd = ttk.Button(dynamic_frame, text = "Update", style="Accent.TButton",
+                        command=lambda frame=dynamic_frame :\
+                                utils.template_handler.templ_handler('menu_tables', frame))
+
     lbl_title.grid(column=1, row=0, padx=20, pady=10)
-    button_del.grid(column=0, row=2, padx=20, pady=10)
+    button_back.grid(column=0, row=2, padx=20, pady=10)
     button_upd.grid(column=3, row=2, padx=20, pady=10)
+
