cbbe3b731a76dcc4c4a811ba242a023a7059e487
SantiagoLopezUV <santiago.lopez.ramirez@correounivalle.edu.co>
2023-12-11 22:24:24 -0500

    Se Crea Base de Datos Pedidos

--- templates/order_templates/delete_order.py
+++ templates/order_templates/delete_order.py
@@ -1,87 +1,68 @@
 from tkinter import ttk
 from tkinter import messagebox
 import utils.template_handler
-import databases.db_tables as tables
-import databases.db_dishes as dbdish
-#from templates.dish_templates.delete_dish_template import columns_names
+import databases.db_orders as order
 
-def warning(dynamic_frame, tree_dish, tree_table):
+
+def warning(dynamic_frame, tree_order):
     try:
-        option_dish = tree_dish.selection()
-        option_table = tree_table.selection()
-        if option_dish and option_table:
+        option = tree_order.selection()
+        if option:
             answer = messagebox.askokcancel("Advertencia",
                                             "¿Seguro quieres ELIMINAR este pedido de forma permanente?")
             if answer:
-                
+                del_order = tree_order.item(option)["values"]  # Guardamos los valores de la seleccion escogida
+                delete_order(del_order[0])
                 utils.template_handler.templ_handler('del_order',
                                                      dynamic_frame)
+            else:
+                messagebox.showerror('Cancelado',
+                                    "No se pudo realizar el proceso")
         else:
-            messagebox.showerror('Cancelado',
-                                 "No se pudo realizar el proceso")
+            messagebox.showerror('Incompleto',
+                             "Seleccione un pedido para Eliminar")
     except Exception:
         messagebox.showerror('Incompleto',
                              "Seleccione un pedido para Eliminar")
 
+
+def delete_order(index):
+    database = order.orders()  # Llamamos la matriz
+    for i in database:  # Recorremos la matriz
+        if index == i[0]:  # Buscamos el valor del indice
+            order.orders.remove(i)  # Eliminamos ese indice
+
+
 def delete_order_template(dynamic_frame):
-    title_dish = ("Codigo Plato")
-    title_tables = ('N. Mesa')
     lbl_title = ttk.Label(dynamic_frame,
-                        text="Pedidos",
-                        font=("default", 12, "bold"))    
-    
-    
-    list_table = ttk.Treeview(dynamic_frame, columns=title_tables, show="headings")
-    list_table.heading(column=9, text=title_tables)
-    list_dish = ttk.Treeview(text=title_dish)
-    
-    for i in tables.tables_reserves:
-        list_table.insert("", "end", values=(i[0],))
-    
-    for j in dbdish.db_matrix:
-        list_dish.insert("", "end", values=(j[0],))
-    
-    
-    
+                        text="Eliminar Pedido",
+                        font=("default",12, "bold"))
 
-    
-    # if columns_names == 'Código':
-    #     tree_dish.heading(text=columns_names)
-    # tree_dish.column(0, anchor="center", width=55)
-    # tree_table.column(0, anchor="center", width=15)
-    
-    # list_dishes = 
-    # list_tables = 
-    
-    # for row in list_dishes:
-    #     tree_table.insert("", "end", values=row)
-    
-    # for row in list_tables:
-    #     tree_table.insert("", "end", values=row)
-    
-    # scrollbar_dish = ttk.Scrollbar(dynamic_frame, orient="vertical", command=tree_dish.yview)
-    # tree_dish.configure(yscrollcommand=scrollbar_dish.set)
-    # scrollbar_table = ttk.Scrollbar(dynamic_frame, orient="vertical", command=tree_table.yview)
-    # tree_table.configure(yscrollcommand=scrollbar_table.set)
-    
-    
-    button_del = ttk.Button(dynamic_frame,
-                            text="Eliminar",
-                            style="Accent.TButton",
-                            command=lambda: warning(dynamic_frame,
-                                                    list_dish,
-                                                    list_table))
+    tree_order = ttk.Treeview(dynamic_frame, columns=order.title_orders,
+                        show="headings", height=9)
 
-    button_back = ttk.Button(dynamic_frame, text="Atrás",
-                            command=lambda: utils.template_handler.\
-                            templ_handler('order_menu', dynamic_frame))
+    for col in order.title_orders:
+        tree_order.heading(col, text=col)
+        tree_order.column(col, anchor="center", width=55)
+    data_base_for_order = order.orders
+    for row in data_base_for_order:
+        tree_order.insert("", "end", values=row)
 
-
-    lbl_title.grid(column=0, row=0, columnspan=2, padx=40, pady=10)
+    scrollbar_y = ttk.Scrollbar(dynamic_frame, orient="vertical", command=tree_order.yview)
+    tree_order.configure(yscrollcommand=scrollbar_y.set)
     
-    list_dish.grid(column=0, row=1, columnspan=2, padx=(15, 0), pady=10)
-    list_table.grid(column=2, row=1, columnspan=2, padx=(15, 0), pady=10)
-    # scrollbar_dish.grid(row=1, column=1, padx=(0, 15), pady=10, sticky="ns")
-    #scrollbar_table.grid(row=1, column=3, padx=(0, 15), pady=10, sticky="ns")
-    button_back.grid(column=0, row=5, sticky="e", padx=(0, 10), pady=15)
-    button_del.grid(column=1, row=5, sticky="w", padx=(10, 0), pady=15)
+    button_del = ttk.Button(
+        dynamic_frame,
+        text="Eliminar",
+        style="Accent.TButton",
+        command=lambda :warning(dynamic_frame, tree_order))
+    button_back = ttk.Button(
+        dynamic_frame,
+        text="Atrás",
+        command=lambda frame=dynamic_frame: utils.template_handler.templ_handler('order_menu',frame))
+
+    lbl_title.grid(column=0, row=0, columnspan=2, pady=10)
+    tree_order.grid(column=0, row=1, columnspan=2, padx=(15, 0), pady=10)
+    scrollbar_y.grid(row=1, column=2, padx=(0, 15), pady=10, sticky="ns")
+    button_back.grid(column=0, row=2, sticky="e", padx=(0, 10), pady=15)
+    button_del.grid(column=1, row=2, sticky="w", padx=(10, 0), pady=15)
