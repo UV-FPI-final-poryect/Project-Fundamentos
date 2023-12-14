34cd74af1ff9ecdd6024cec480071b29e14e1252
redohawk <131167893+redohawk@users.noreply.github.com>
2023-12-14 02:20:55 -0500

    Update main_menu_template.py
    Se agrega documentación

--- templates/main_templates/main_menu_template.py
+++ templates/main_templates/main_menu_template.py
@@ -3,13 +3,33 @@ import utils.template_handler
 import data_access_tools.user_auth as tools_users
 
 
+"""
+This template displays the menu where the user can access to the management
+options and log out.
+
+Imports:
+- 'ttk' module from the 'tkinter' library for creating the Graphical
+User Interface (GUI).
+- 'user_auth' is the module which manage the user credentials.
+- 'templ_handler' method from the 'template_handler' module.
+
+Purpose:
+- Generates the main menu template asigned to the dynamic_frame.
+- Utilizes 'ttk' for creating graphical elements in the GUI.
+- Implements 'templ_handler' for handling the templates in the application.
+- Shows the management and log out options
+"""
+
+
 def logout_action(dynamic_frame):
+    #This function allow the user to log out whenever he/she wants
     tools_users.logout_user()
     print(tools_users.token)
     utils.template_handler.templ_handler('initial', dynamic_frame)
 
 
 def main_menu_template(dynamic_frame):
+    #Generates the structure for the main menu template
     lbl_title = ttk.Label(dynamic_frame,
                           text="Bienvenido",
                           font=("default", 12, "bold"))
