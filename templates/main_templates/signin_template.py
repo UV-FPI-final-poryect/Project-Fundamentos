0ceebb581c9dcfec7a8e77f8ec5fb00c3a01b1d6
redohawk <131167893+redohawk@users.noreply.github.com>
2023-12-14 02:43:36 -0500

    Se agrega documentación
    Se agrega documentación

--- templates/main_templates/signin_template.py
+++ templates/main_templates/signin_template.py
@@ -4,12 +4,53 @@ import re
 import utils.template_handler
 import data_access_tools.user_auth as tools_users
 
+
+"""
+This template is generated to gather and verify the user sign in info
+before it will be send to the user authentication script (user_auth.py) 
+and save it.
+
+Imports:
+- 'ttk' module from the 'tkinter' library for creating the Graphical
+User Interface (GUI).
+- 'messagebox' module from the 'tkinter' library which creates a focused
+top level frame with an advise to the user.
+- 're' is the regular expression library for python where we can use 
+several methods to verify if a string accomplish a designed pattern.
+- 'user_auth' is the module which manage the user credentials.
+- 'templ_handler' method from the 'template_handler' module.
+
+Purpose:
+- Generates the sign in template asigned to the dynamic_frame.
+- Utilizes 'ttk' for creating graphical elements in the GUI.
+- Implements 'templ_handler' for handling the templates in the application.
+- Verify that the credentials are right before manage it in the backend.
+
+ Args:
+        user_email (String): The verified user email which will be send to
+        user_auth.
+        user_pass (String): The user password which will be send to
+        user_auth.
+        user_confirm_pass (String): The user confirm password which will be 
+        used to validate if the user password is ok.
+"""
+
+
 user_email = ''
 user_pass = ''
 user_confirm_pass = ''
 
 
 def warning(dynamic_frame):
+    """
+    In this function is where the exceptions will be handled when something 
+    breaks up and displays a confirm message box before saving the user
+    credentials.
+    
+    Params:
+        dynamic_frame (widget): The frame object to receive the 
+        selected template.
+    """
     try:
         result = messagebox.askokcancel("Confirmación", "¿Son correctos los datos que ingresaste?")
         if result:
@@ -26,6 +67,8 @@ def warning(dynamic_frame):
 
 
 def save_process():
+    #Verified if the global variables are ok and creates a list with them 
+    #which it will passed to the sign in method of user_auth
     global user_email
     global user_pass
     global user_confirm_pass
@@ -44,6 +87,15 @@ def save_process():
 
 
 def catch_user_email(var):
+    """
+    Here validates the email entered in the entry_email widget using the 
+    're' library and if everything is ok, the entered email is assigned 
+    to the user_email global variable.
+
+    Parameters:
+        var (String): is the parameter given by the validatecommand to 
+        be evaluated.
+    """
     global user_email
     pattern = r'[A-Za-z0-9._%+-]+@{1}(gmail|hotmail|yahoo|outlook|correounivalle.edu){1}\.(com|co){1}'
     if re.fullmatch(pattern, var) is None or len(var) > 50:
@@ -58,6 +110,15 @@ def catch_user_email(var):
 
 
 def catch_user_pass(var):
+    """
+    Here validates the password entered in the entry_pass widget using the 
+    're' library and if everything is ok, the entered password is assigned 
+    to the user_pass global variable.
+
+    Parameters:
+        var (String): is the parameter given by the validatecommand to be
+        evaluated.
+    """
     global user_pass
     if (re.search(r'[a-z]', var) is None or
             re.search(r'[A-Z]', var) is None or
@@ -79,6 +140,14 @@ def catch_user_pass(var):
 
 
 def catch_confirm_pass(var):
+    """
+    Here confirms if the password entered in the entry_confirm_pass and
+    entry_pass widgets are the same.
+
+    Parameters:
+        var (String): is the parameter given by the validatecommand to be 
+        evaluated.
+    """
     global user_confirm_pass
     global user_pass
     if var != user_pass or user_pass == '':
@@ -91,6 +160,7 @@ def catch_confirm_pass(var):
 
 
 def signin_template(dynamic_frame):
+    #Generates the structure for the sign in template
     entry_box_width = 35
 
     lbl_title = ttk.Label(dynamic_frame,
