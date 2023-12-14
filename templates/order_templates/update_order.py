60515e8b3dc7264fc0fd2ad05e758811ed291fc2
redohawk <131167893+redohawk@users.noreply.github.com>
2023-12-14 03:28:25 -0500

    Se agrega documentación

--- templates/order_templates/update_order.py
+++ templates/order_templates/update_order.py
@@ -5,7 +5,25 @@ import databases.db_orders as order
 import templates.order_templates.order_upd as change
 
 
+"""
+This module imports libraries such as 'tkinter' - 'ttk', and 
+'tkinter' - 'messagebox' for notifications, as well as widgets 
+and functionalities for table organization within the dynamic_frame. 
+Additionally, it imports modules like 'templates.order_templates.order_upd'
+for displaying a more confortable interface to update the dishes.
+"""
+
+
 def warning(dynamic_frame, tree_order):
+    """
+    This method prompts the user to confirm their selected option and
+    utilizes exceptions to prevent program interruption.
+
+    Args:
+        dynamic_frame (frame - widget): The dynamic frame being configured 
+        for user interaction.
+        tree_order (treeview - widget): The table with the order items.
+    """
     try:
         option = tree_order.selection()
         if option:
@@ -26,6 +44,7 @@ def warning(dynamic_frame, tree_order):
 
 
 def update_order_template(dynamic_frame):
+    #Generates the structure for the order creation template
     lbl_title = ttk.Label(dynamic_frame,
                         text="Actualizar Pedido",
                         font=("default",12, "bold"))
