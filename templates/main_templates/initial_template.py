4c6afc930785438ff24d84405d760852a449b54c
redohawk <131167893+redohawk@users.noreply.github.com>
2023-12-13 22:25:24 -0500

    Update initial_template.py
    Se agregó documentación

--- templates/main_templates/initial_template.py
+++ templates/main_templates/initial_template.py
@@ -1,13 +1,30 @@
 from tkinter import ttk
 import textwrap
-
-
 import utils.template_handler
 
+"""
+This module is the first template that appears when the user executes the aplication.
+
+Imports:
+- 'ttk' module from the 'tkinter' library for creating the Graphical
+User Interface (GUI).
+- 'textwrap' is used to dedent large strings.
+- 'templ_handler' method from the 'template_handler' module.
+
+Purpose:
+- Generates the primary template asigned to the dynamic_frame.
+- Utilizes 'ttk' for creating graphical elements in the GUI.
+- Implements 'templ_handler' for handling the templates in the application.
+"""
 
 def initial_template(dynamic_frame):
-    # Primera Ventana | Inicio
+    """
+    Create the widgets for the initial_template
 
+    Args:
+        dynamic_frame (widget): The frame object to receive the 
+        selected template.
+    """
     intro_txt = textwrap.dedent("""
                     Nuestro restaurante es un lugar donde ofrecemos
                     una variedad de platos deliciosos y recursos
